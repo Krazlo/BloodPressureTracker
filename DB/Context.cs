56f082256c253777bbd2e2f87ebab21da12a8f2f
Kristian <krazlo878@gmail.com>
2024-11-15 16:33:47 +0700

    db

---
+++ DB/Context.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Metrics;
+using Helpers.Entities;
+
+namespace DB
+{
+    public class Context : DbContext
+    {
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Measurement> Measurements { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                "Server=localhost;Database=mysqlDB;User=sa;Password=S3cr3tP4sSw0rd#123;",
+                new MySqlServerVersion(new Version(8, 0, 23))
+            );
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Patient>()
+                .HasKey(p => p.SSN);
+
+            modelBuilder.Entity<Measurement>()
+                .HasKey(m => m.ID);
+        }
+    }
+}
---
+++ Helpers/Entities/Measurement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers.Entities
+{
+    public class Measurement
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public int Systolic { get; set; }
+
+        public int Diastolic { get; set; }
+
+        public bool Seen { get; set; }
+
+        [ForeignKey("Patient")]
+        [StringLength(10)]
+        public string PatientSSN { get; set; }
+
+        public virtual Patient Patient { get; set; }
+    }
+}
