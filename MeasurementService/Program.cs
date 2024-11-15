f5b28446f5be39c713356df3e90ebdc6f401a020
Zr0den <94046425+Zr0den@users.noreply.github.com>
2024-11-15 17:30:33 +0700

    Initial Service

--- MeasurementService/Program.cs
+++ MeasurementService/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DB;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MeasurementService;
+
+var serviceProvider = new ServiceCollection().AddDbContext<Context>(options =>
+options.UseMySql(
+                "Server=localhost;Database=mysqlDB;User=sa;Password=S3cr3tP4sSw0rd#123;", new MySqlServerVersion(new Version(8, 0, 23))))
+    .AddScoped<IMService, MService>()
+    .BuildServiceProvider();
+
