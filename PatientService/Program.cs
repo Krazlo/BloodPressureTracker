13e89536eaf0505fac9ac0456bd9e9fd4603c187
Zr0den <94046425+Zr0den@users.noreply.github.com>
2024-11-18 15:10:08 +0700

    networking & docker

--- PatientService/Program.cs
+++ PatientService/Program.cs
@@ -6,18 +6,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddDbContext<Context>(options =>
-    options.UseMySql(
-        builder.Configuration.GetConnectionString("DefaultConnection"),
-        new MySqlServerVersion(new Version(8, 0, 30))
-    ));
-builder.Services.AddScoped<IPatientRepository, PatientRepository>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<Context>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(8, 0, 30))
+    ));
+builder.Services.AddScoped<IPatientRepository, PatientRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
